b3dd04917ae783053f54a171af18589d0b018e30
Jihwan Moon <mnjihw@naver.com>
2023-12-19 02:08:57 +0900

    67. Add Binary 해답

--- LeetCode/67_Add_Binary.cs
+++ LeetCode/67_Add_Binary.cs
@@ -4,20 +4,21 @@ public class Solution
     {
         var stack = new Stack<char>();
         int carryBit = 0;
+        int n = Math.Max(a.Length, b.Length);
 
-        for(int i = 0; i < Math.Max(a.Length, b.Length); ++i)
+        for(int i = 0; i < n; ++i)
         {
             var aIndex = a.Length - i - 1;
             var bIndex = b.Length - i - 1;
-            var aNum = aIndex >= 0 ? a[aIndex] ^ '0' : 0;
-            var bNum = bIndex >= 0 ? b[bIndex] ^ '0' : 0;
+            var aChar = aIndex >= 0 ? a[aIndex] : '0';
+            var bChar = bIndex >= 0 ? b[bIndex] : '0';
 
-            stack.Push((char)(aNum ^ bNum ^ carryBit ^ '0'));
-            carryBit = aNum & bNum | aNum & carryBit | bNum & carryBit;
+            stack.Push((char)(aChar ^ bChar ^ carryBit ^ '0'));
+            carryBit = (aChar & bChar | aChar & carryBit | bChar & carryBit) ^ '0';
         }
 
         if(carryBit == 1)
-            stack.Push((char)(carryBit ^ '0'));
+            stack.Push('1');
         return new string(stack.ToArray());
     }
 }
