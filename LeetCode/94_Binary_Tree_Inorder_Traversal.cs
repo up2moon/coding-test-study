7a52e6c0f4e44b6ed0398311590d39e1a8db3abf
mnjihw <mnjihw@naver.com>
2023-12-19 19:45:10 +0900

    94. Binary Tree Inorder Traversal 해답

---
+++ LeetCode/94_Binary_Tree_Inorder_Traversal.cs
@@ -0,0 +1,33 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public List<int> list = new List<int>();
+
+    public void Visit(TreeNode node)
+    {
+        if (node == null)
+            return;
+
+        Visit(node.left);
+        list.Add(node.val);
+        Visit(node.right);
+    }
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        Visit(root);
+        return list;
+    }
+}
