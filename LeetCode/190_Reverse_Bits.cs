f870a80cabce04dc7976f6e55f1f7ca26ba4898a
Jihwan Moon <dev.moonjihwan@gmail.com>
2024-02-13 19:57:11 +0900

    190. Reverse Bits 해답

---
+++ LeetCode/190_Reverse_Bits.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public uint reverseBits(uint n)
+    {
+        uint result = 0;
+        var list = new List<uint>();
+
+        while (n > 0)
+        {
+            uint remainder = n % 2;
+            list.Add(remainder);
+            n /= 2;
+        }
+
+        int count = list.Count;
+        for (int i = 0; i < 32 - count; ++i)
+        {
+            list.Add(0);
+        }
+        count = list.Count;
+        for (int i = 0; i < count; ++i)
+        {
+            int idx = count - 1 - i;
+            result += (uint)Math.Pow(2, i) * list[idx];
+        }
+        return result;
+    }
+}
