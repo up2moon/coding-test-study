5f2b159ccfa054c72fa42a1f49447a1e65b004a1
Jihwan Moon <dev.moonjihwan@gmail.com>
2024-02-19 08:20:41 +0900

    263. Ugly Number 해답

---
+++ LeetCode/263_Ugly_Number.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public bool IsUgly(int n)
+    {
+        if (n == 0)
+            return false;
+
+        while (true)
+        {
+            if (n % 5 == 0)
+                n /= 5;
+            else if (n % 3 == 0)
+                n /= 3;
+            else if (n % 2 == 0)
+                n /= 2;
+            else
+                break;
+        }
+
+        return n == 1;
+    }
+}
