ee0ae4b9ae8f72e41f4e683b482c87d3f85ba01d
Jihwan Moon <jhmoon256@gmail.com>
2025-09-01 21:38:22 +0900

    2. Add Two Numbers 해답

--- LeetCode/2_Add_Two_Numbers.cs
+++ LeetCode/2_Add_Two_Numbers.cs
@@ -1,23 +1,26 @@
-public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+public class Solution
 {
-    ListNode dummy = new ListNode(0);
-    ListNode cur = dummy;
-
-    int carry = 0;
-    while (l1 != null || l2 != null || carry != 0)
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        int x = (l1 != null) ? l1.val : 0;
-        int y = (l2 != null) ? l2.val : 0;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
 
-        int sum = x + y + carry;
-        carry = sum / 10;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
 
-        cur.next = new ListNode(sum % 10);
-        cur = cur.next;
+            int sum = x + y + carry;
+            carry = sum / 10;
 
-        if (l1 != null) l1 = l1.next;
-        if (l2 != null) l2 = l2.next;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
     }
-
-    return dummy.next;
 }
